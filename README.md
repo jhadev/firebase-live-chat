73a4e7a50d2a18c038c4d35ecc1698499f31437f
JA <joshappeldev@gmail.com>
2019-03-21 22:38:03 -0400

    readme

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ### A real time chat application using Google Firebase and Google OAuth.
 
+Updated to allow links and images through urls. Supports inline png, gif, jpg, and jpeg images. In current state the hyperlinks that show up when an image is posted are dead, but clicking the image will open the url in a new tab.
+
 [Chat Here](https://jhadev.github.io/firebase-live-chat/)
 
 ![live-chat-demo](https://user-images.githubusercontent.com/42519030/54730812-4602a480-4b61-11e9-8086-9e211335a2d8.jpg)
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -200,7 +200,7 @@ $(document).ready(() => {
       let emailAddressPattern = /[\w.]+@[a-zA-Z_-]+?(?:\.[a-zA-Z]{2,6})+/gim;
       let imgUrlPattern = /(https?:\/\/.*\.(?:png|jpg|gif|jpeg))/i
 
-      let img = this.replace(urlPattern, `$1<a class="msg-link" href="$&" target="_blank"><img class="msg-img img-fluid rounded" src="$&"></a>`)
+      let img = this.replace(urlPattern, `$1<a class="msg-link" href="$&" target="_blank"><img class="msg-img rounded" src="$&"></a>`)
       return this
         .replace(urlPattern, `<a class="msg-link" href="$&" target="_blank">$&</a>`)
         .replace(pseudoUrlPattern, '$1<a class="msg-link" href="http://$2" target="_blank">$2</a>')
